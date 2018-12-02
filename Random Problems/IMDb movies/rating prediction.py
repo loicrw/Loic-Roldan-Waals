ef0006e8000340f72c53728143beb597bd1665b0
loicrw <39776564+loicrw@users.noreply.github.com>
2018-12-02 21:31:44 +0100

    Update rating prediction.py
    added comments for clearer understanding

--- Random Problems/IMDb movies/rating prediction.py
+++ Random Problems/IMDb movies/rating prediction.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import RandomizedSearchCV
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from matplotlib import pyplot as plt
     
 
 
@@ -23,7 +24,7 @@ movies = pd.read_csv(wd + "complete movie database.csv", sep = ';').drop(['Unnam
 X = movies[movies.columns[5:]]
 y = movies['Rating']
 
-#deal with missing values by indicating them with a column
+#deal with missing values by indicating them with another column
 X['Missing metascore'] = list(X['metascore'].isna())
 X['Missing IMDb score'] = list(X['IMDb rating'].isna())
 X['Missing year'] = list(X['year'].isna())
@@ -34,7 +35,7 @@ for i in colMissing:
     X[i].fillna(np.mean(X[i]), inplace = True)
 
 '''
-### Scaling decreases performance
+### Scaling decreases performance and is thus not commented out
 # scale the data
 scaler = MinMaxScaler()
 scaler.fit(X)
@@ -44,9 +45,10 @@ X = scaler.transform(X)
 X = pd.DataFrame(X, columns = cols)
 '''
 
+# after manually checking only these variables seem to have an impact on my rating of the movies, thus only these are selected
 X = X[['IMDb rating','year','metascore','Missing year', 'Missing metascore', 'Action', 'Animation', 'Mystery', 'PG', 'R']]
 
-# Determining optimal parameters for SVR
+# Determining optimal parameters for SVR; conclusion: kernel='linear',gamma=0.001, C=0.03
 parameters = {'gamma': [0.001, 0.0001, 0.0005, 0.003, 0.005], 'C': [0.01, 0.03, 0.001, 0.003], 'kernel': ['linear', 'rbf']}
 sm = svm.SVR()
 gridsm = RandomizedSearchCV(sm, param_distributions =parameters, n_iter = 40, cv=3, scoring='r2')
@@ -54,7 +56,7 @@ gridsm.fit(X, y)
 gridsm.best_params_
 scoresm = gridsm.best_score_
 
-# Determining optimal parameters for NB
+# Determining optimal parameters for NB; conclusion: n_iter=1
 parameters = {'n_iter': [10,1,5,15,12]}
 nb = BayesianRidge()
 gridnb = RandomizedSearchCV(nb, param_distributions =parameters, n_iter = 5, cv=3, scoring='r2')
@@ -63,7 +65,7 @@ scorenb = gridnb.best_score_
 gridnb.best_params_
 scoresnb = (cross_val_score(nb, X, y, cv=3, scoring='r2')).mean()
 
-# Determining optimal parameters for linear regression
+# Determining optimal parameters for linear regression; conclusion: n_jobs=1, fit_intercept=1
 parameters = {'fit_intercept': [1,0], 'n_jobs': [1,5,10,50,100]}
 lr = LinearRegression()
 gridlr = RandomizedSearchCV(lr, param_distributions =parameters, n_iter = 10, cv=3, scoring='r2')
@@ -72,8 +74,8 @@ scorelr = gridlr.best_score_
 gridlr.best_params_
 scoreslr = (cross_val_score(lr, X, y, cv=3, scoring='r2')).mean()
 
-# Determining optimal parameters for random forest
-parameters = {'n_estimators': [1,2,3,5,4,6,7], 'max_depth': [1], 'max_features': [None], 'max_leaf_nodes':[5,4,6]}
+# Determining optimal parameters for random forest; conclusion: n_estimators = 4, max_depth = 1, max_leaf_nodes=6, max_features=None
+parameters = {'n_estimators': [1,2,3,5,4,6,7], 'max_depth': [1, 3, 10, 15, 20], 'max_features': [None], 'max_leaf_nodes':[5,4,6, 10]}
 rf = RandomForestRegressor()
 gridrf = RandomizedSearchCV(rf, param_distributions =parameters, n_iter = 21, cv=3, scoring='r2')
 gridrf.fit(X,y)
@@ -81,7 +83,7 @@ scorerf = gridrf.best_score_
 gridrf.best_params_
 scoresrf = (cross_val_score(rf, X, y, cv=3, scoring='r2')).mean()
 
-# Determining optimal parameters for gradient boosting
+# Determining optimal parameters for gradient boosting; conclusion: learning_rate = 0.05, n_estimators = 30, max_leaf_nodes = 5, max_features = None
 parameters = {'learning_rate': [0.03,0.05,0.07], 'n_estimators': [30,50,70], 'max_features': [None], 'max_leaf_nodes':[5,4,6], 'max_depth': [1,2,5,10]}
 gb = GradientBoostingRegressor()
 gridgb = RandomizedSearchCV(gb, param_distributions =parameters, n_iter = 108, cv=3, scoring='r2')
@@ -92,15 +94,17 @@ gb = GradientBoostingRegressor(learning_rate = 0.01, n_estimators = 75)
 scoresgb = (cross_val_score(gb, X, y, cv=3, scoring='r2')).mean()
 
 
-#determine the score when multiple models are combined
+# determine the score when multiple models are combined and which combination of models leads to optimal performance; conclusion: sm and lr
 def multipleAgg(X, y):
     aggscores = []
+    
+    # run the aggregated model 300 times with different random states
     for i in range(300):
         xtrain, xtest, ytrain, ytest= train_test_split(X, y, test_size=0.3, random_state=i)
         sm = svm.SVR(kernel='linear',gamma=0.001, C=0.03).fit(xtrain, ytrain)
         #nb = BayesianRidge(n_iter=1).fit(xtrain, ytrain)
         lr = LinearRegression(n_jobs=1, fit_intercept=1).fit(xtrain, ytrain)
-        #rf = RandomForestRegressor(n_estimators = 200, max_depth = 1, max_leaf_nodes=6, max_features=None).fit(xtrain, ytrain)
+        #rf = RandomForestRegressor(n_estimators = 4, max_depth = 1, max_leaf_nodes=6, max_features=None).fit(xtrain, ytrain)
         #gb = GradientBoostingRegressor(learning_rate = 0.05, n_estimators = 30, max_leaf_nodes = 5, max_features = None).fit(xtrain, ytrain)
         
         predsm = sm.predict(xtest)
@@ -116,42 +120,35 @@ def multipleAgg(X, y):
 ascoreAgg, aggscores = multipleAgg(X, y)
 
 
-# visualisation some things
-
-from matplotlib import pyplot as plt
+# visualisation for data exploration
+# label all r2 scores under 0 as outliers and exclude them (less then 30 outliers
 data = [x for x in aggscores if x > 0]
 np.median(data)
 np.mean(data)
 
-# fixed bin size
+# show a histogram of fixed bin size of the distribution of the r2 scores
 bins = np.arange(-100, 100, 0.03) # fixed bin size
-
 plt.xlim([min(data)-0.05, max(data)+0.05])
-
 plt.hist(data, bins=bins, alpha=0.5)
 plt.title('Distribution of r2 scores for multipleAgg')
 plt.xlabel('r2 score')
 plt.ylabel('count')
 
 
-
+#Q what is the correlation coefficient between the metascore, the IMDb rating and my rating?
 (movies['metascore'].corr(movies['Rating']))**2
+(movies['IMDb rating'].corr(movies['Rating']))**2
 
-movies['metascore']
-r2_score(y, X['IMDb rating'])
-y.corr(X['IMDb rating'])**2
-
-
+#Q what do the scatter plots look like?
 plt.scatter(y,X['IMDb rating'])
 plt.scatter(y,X['metascore'])
 plt.scatter(X['IMDb rating'], X['metascore'])
-X['metascore'].corr(X['IMDb rating'])
 
-
+# 3d scatter where the color is my score (lighter is higher)
 plt.scatter(X['metascore'], X['IMDb rating'], c=y)
 plt.show()
 
-''' MISCELANEOUS TRIALS VERSIONS  
+''' MISCELANEOUS TRIAL VERSIONS  
 xtrain, xtest, ytrain, ytest= train_test_split(X, y, test_size=0.33, random_state=3)
 sm = svm.SVR(gamma=0.01, C=0.1).fit(xtrain, ytrain)
 nb = BayesianRidge().fit(xtrain, ytrain)
@@ -163,9 +160,6 @@ predrf = rf.predict(xtest)
 
 aggpred = [sum(e)/len(e) for e in zip(*[predsm, prednb, predrf])]
 aggscore = r2_score(ytest, aggpred)
-
-
-(movies['IMDb rating'].corr(movies['Rating']))**2
 '''
 
 
